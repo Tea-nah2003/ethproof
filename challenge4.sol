0782bba2890b6088b09f0f5f8c20deedada68073
Teena Neupane <127821631+Tea-nah2003@users.noreply.github.com>
2024-06-09 15:29:35 +0530

    Add files via upload

---
+++ challenge4.sol
@@ -0,0 +1,21 @@
+//SPDX-License-Identifier:MIT
+pragma solidity ^0.8.26;
+contract calculations
+{
+    function add(uint a,uint b)external pure returns(uint)
+    {
+        return a+b;
+    }
+     function subtract(uint a,uint b)external pure returns(uint)
+    {
+        return a-b;
+    }
+     function multiply(uint a,uint b)external pure returns(uint)
+    {
+        return a*b;
+    }
+     function divide(uint a,uint b)external pure returns(uint)
+    {
+        return a/b;
+    }
+}
